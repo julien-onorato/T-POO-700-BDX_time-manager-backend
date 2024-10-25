aec393ffa201345303cbc92db3fc531a33261669
AndyDCK <andy.decock64@icloud.com>
2024-10-25 12:42:32 +0200

    fix: change localhost to vps host

--- config/config.exs
+++ config/config.exs
@@ -13,8 +13,8 @@ config :time_manager,
 
 # Configures the endpoint
 config :time_manager, TimeManagerWeb.Endpoint,
-  #url: [host: "134.209.208.89"], # A décommenter lors du déploiement
-  url: [host: "localhost"],
+  url: [host: "134.209.208.89"], # A décommenter lors du déploiement
+  #url: [host: "localhost"],
   adapter: Bandit.PhoenixAdapter,
   render_errors: [
     formats: [json: TimeManagerWeb.ErrorJSON],
